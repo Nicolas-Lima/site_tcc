569e7c0f85e6e5c1d3ef5db2f542b4344bc9e556
Nícolas Lima <91204839+Nicolas-Lima@users.noreply.github.com>
2023-07-21 16:51:28 -0300

    Estilização da página de erro

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Error() {
-
-    return (
-        <div>
-            Error
-        </div>
-    )
-
+  return (
+    <div className="d-flex flex-column align-items-center text-center mt-2-3rem px-2 px-sm-0 fs-5">
+      <h1 className="text-danger fw-normal mb-5">
+        Essa página não existe!
+      </h1>
+      <Link to="/">
+        Voltar para a página inicial
+      </Link>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
