cfb8a0c31c624099b44e620d914af3475dad96f7
Nícolas Lima <91204839+Nicolas-Lima@users.noreply.github.com>
2023-07-21 20:35:09 -0300

    Estilização da página de ERRO

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 function Error() {
   return (
-    <div className="d-flex flex-column align-items-center text-center mt-5 px-2 px-sm-0">
-      <h1 className="text-danger fw-normal mb-5">
-        Essa página não existe!
+    <div className="d-flex flex-column align-items-center mt-5 ps-3 pe-2">
+      <h1 className="text-secondary fw-semibold mt-1">
+        Não conseguimos encontrar esta página!
       </h1>
-      <Link to="/" className="fs-5">
+      <Link to="/" className="fs-5 p-3 rounded">
         Voltar para a página inicial
       </Link>
     </div>
