080e310b5d49079cc8291a1c4b3b55cda3e2d060
Nícolas Lima <91204839+Nicolas-Lima@users.noreply.github.com>
2023-07-22 15:37:42 -0300

    Retirada da biblioteca react toastify

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import RoutesApp from "./routes";
 import AuthProvider from "./contexts/auth";
 
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 import "./css/bootstrap-utilities.css";
 import "./css/pico-bootstrap-grid.css";
 import "./css/app.css";
@@ -12,7 +9,6 @@ import "./css/app.css";
 function App() {
   return (
     <BrowserRouter>
-      <ToastContainer autoClose={3000} />
       <AuthProvider>
         <RoutesApp />
       </AuthProvider>
