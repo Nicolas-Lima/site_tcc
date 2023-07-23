7f231ae5a3a728bd96f4abbd6db9edb2c17bc84d
Nícolas Lima <91204839+Nicolas-Lima@users.noreply.github.com>
2023-07-23 15:29:09 -0300

    generalUtils atualizado

--- src/utils/generalUtils.js
+++ src/utils/generalUtils.js
@@ -21,9 +21,25 @@ function capitalizeFirstLetter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+function mpsToKmph(mps) {
+  // Convert meters per second to kilometers per hour
+  const kmph = mps * 3.6;
+  return parseFloat(kmph.toFixed(2));
+}
+
+function roundToNearestInteger(number) {
+  const decimalPart = number - Math.floor(number);
+  if (decimalPart >= 0.5) {
+    return Math.ceil(number);
+  }
+  return Math.floor(number);
+}
+
 export {
   hasUppercase,
   hasSpecialCharacter,
   hasNumber,
   capitalizeFirstLetter,
+  mpsToKmph,
+  roundToNearestInteger,
 };
